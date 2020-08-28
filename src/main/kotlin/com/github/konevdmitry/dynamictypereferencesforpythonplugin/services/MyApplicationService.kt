c4a413f59068749d861a4eac48fe60a27aa0a552
GitHub Action <action@github.com>
2020-08-28 22:12:20 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/konevdmitry/dynamictypereferencesforpythonplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.konevdmitry.dynamictypereferencesforpythonplugin.services
+
+import com.github.konevdmitry.dynamictypereferencesforpythonplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/konevdmitry/dynamictypereferencesforpythonplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/konevdmitry/dynamictypereferencesforpythonplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.konevdmitry.dynamictypereferencesforpythonplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.konevdmitry.dynamictypereferencesforpythonplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
