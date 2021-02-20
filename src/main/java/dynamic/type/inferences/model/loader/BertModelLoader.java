0ec101ee37ae981babacc069ce6bd39e93226458
DmitryKonev <Konev1999D@mail.ru>
2021-02-20 21:17:24 +0300

    moving token to resources (and pushing for now)

--- src/main/java/dynamic/type/inferences/model/loader/BertModelLoader.java
+++ src/main/java/dynamic/type/inferences/model/loader/BertModelLoader.java
@@ -6,6 +6,8 @@ import com.dropbox.core.v2.DbxClientV2;
 import com.dropbox.core.v2.files.FileMetadata;
 
 import java.io.*;
+import java.net.URL;
+import java.util.Objects;
 
 public class BertModelLoader {
     final Object sharedObject;
@@ -16,8 +18,14 @@ public class BertModelLoader {
 
     public void loadTo(String pathToLoad) throws IOException, DbxException {
         DbxRequestConfig config = new DbxRequestConfig("BertModelLoader");
-        String token = "zkNb09HCVTAAAAAAAAAAAVb0XG9hL-oQMWzmnieliPzboj8g2UOnCNXeAtNsCdkU";
 
+        URL url = getClass().getClassLoader().getResource("/data/torchBERT/token");
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(
+                        Objects.requireNonNull(url).openStream()));
+
+        String token = reader.readLine();
+        reader.close();
         DbxClientV2 client = new DbxClientV2(config, token);
         OutputStream outputStream = new FileOutputStream(pathToLoad);
         FileMetadata metadata = client
