1e6f7a298712c5713c5b25276a97937a6623e2b7
DmitryKonev <Konev1999D@mail.ru>
2021-05-04 20:40:29 +0300

    java 11 issue

--- build.gradle.kts
+++ build.gradle.kts
@@ -86,17 +86,17 @@ intellij {
 
 tasks {
     withType<JavaCompile> {
-        sourceCompatibility = "1.8"
-        targetCompatibility = "1.8"
+        sourceCompatibility = "11"
+        targetCompatibility = "11"
     }
     listOf("compileKotlin", "compileTestKotlin").forEach {
         getByName<KotlinCompile>(it) {
-            kotlinOptions.jvmTarget = "1.8"
+            kotlinOptions.jvmTarget = "11"
         }
     }
 
     withType<Detekt> {
-        jvmTarget = "1.8"
+        jvmTarget = "11"
     }
 
     patchPluginXml {
